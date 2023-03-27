273108df81311cfb9ff944a9814a34e12e805874
functionRui <1637807375@qq.com>
2023-03-27 23:54:07 +0800

    格式化

--- ConferenceReservation/src/main/java/com/jcxx/saas/common/exception/SaaSExceptionHandler.java
+++ ConferenceReservation/src/main/java/com/jcxx/saas/common/exception/SaaSExceptionHandler.java
@@ -19,41 +19,41 @@ import com.jcxx.saas.common.utils.Result;
  */
 @RestControllerAdvice
 public class SaaSExceptionHandler {
-	private Logger logger = LoggerFactory.getLogger(getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-	/**
-	 * 处理自定义异常
-	 */
-	@ExceptionHandler(SaaSException.class)
-	public Result handleRRException(SaaSException e){
-		Result r = new Result();
-		r.put("code", e.getCode());
-		r.put("msg", e.getMessage());
+    /**
+     * 处理自定义异常
+     */
+    @ExceptionHandler(SaaSException.class)
+    public Result handleRRException(SaaSException e) {
+        Result r = new Result();
+        r.put("code", e.getCode());
+        r.put("msg", e.getMessage());
 
-		return r;
-	}
+        return r;
+    }
 
-	@ExceptionHandler(NoHandlerFoundException.class)
-	public Result handlerNoFoundException(Exception e) {
-		logger.error(e.getMessage(), e);
-		return Result.error(404, "路径不存在，请检查路径是否正确");
-	}
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public Result handlerNoFoundException(Exception e) {
+        logger.error(e.getMessage(), e);
+        return Result.error(404, "路径不存在，请检查路径是否正确");
+    }
 
-	@ExceptionHandler(DuplicateKeyException.class)
-	public Result handleDuplicateKeyException(DuplicateKeyException e){
-		logger.error(e.getMessage(), e);
-		return Result.error("数据库中已存在该记录");
-	}
+    @ExceptionHandler(DuplicateKeyException.class)
+    public Result handleDuplicateKeyException(DuplicateKeyException e) {
+        logger.error(e.getMessage(), e);
+        return Result.error("数据库中已存在该记录");
+    }
 
-	@ExceptionHandler(AuthorizationException.class)
-	public Result handleAuthorizationException(AuthorizationException e){
-		logger.error(e.getMessage(), e);
-		return Result.error("没有权限，请联系管理员授权");
-	}
+    @ExceptionHandler(AuthorizationException.class)
+    public Result handleAuthorizationException(AuthorizationException e) {
+        logger.error(e.getMessage(), e);
+        return Result.error("没有权限，请联系管理员授权");
+    }
 
-	@ExceptionHandler(Exception.class)
-	public Result handleException(Exception e){
-		logger.error(e.getMessage(), e);
-		return Result.error();
-	}
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e) {
+        logger.error(e.getMessage(), e);
+        return Result.error();
+    }
 }
