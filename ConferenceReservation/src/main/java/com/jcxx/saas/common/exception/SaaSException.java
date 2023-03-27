f880a48ca92d41ca5d58f88fcaa0d42c024ce6e7
functionRui <1637807375@qq.com>
2023-03-28 00:01:22 +0800

    格式化

--- ConferenceReservation/src/main/java/com/jcxx/saas/common/exception/SaaSException.java
+++ ConferenceReservation/src/main/java/com/jcxx/saas/common/exception/SaaSException.java
@@ -8,48 +8,48 @@ package com.jcxx.saas.common.exception;
  * 自定义异常
  */
 public class SaaSException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
+    private static final long serialVersionUID = 1L;
+
     private String msg;
     private int code = 500;
-    
+
     public SaaSException(String msg) {
-		super(msg);
-		this.msg = msg;
-	}
-	
-	public SaaSException(String msg, Throwable e) {
-		super(msg, e);
-		this.msg = msg;
-	}
-	
-	public SaaSException(String msg, int code) {
-		super(msg);
-		this.msg = msg;
-		this.code = code;
-	}
-	
-	public SaaSException(String msg, int code, Throwable e) {
-		super(msg, e);
-		this.msg = msg;
-		this.code = code;
-	}
+        super(msg);
+        this.msg = msg;
+    }
 
-	public String getMsg() {
-		return msg;
-	}
+    public SaaSException(String msg, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+    }
 
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
+    public SaaSException(String msg, int code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
 
-	public int getCode() {
-		return code;
-	}
+    public SaaSException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
-	
-	
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+
 }
