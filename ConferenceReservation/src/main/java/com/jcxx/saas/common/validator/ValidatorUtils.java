5fddcd26231d5ad6fb1622b8ffe916432e4997b3
functionRui <1637807375@qq.com>
2023-04-09 23:42:40 +0800

    格式化

--- ConferenceReservation/src/main/java/com/jcxx/saas/common/validator/ValidatorUtils.java
+++ ConferenceReservation/src/main/java/com/jcxx/saas/common/validator/ValidatorUtils.java
@@ -4,17 +4,16 @@
 
 package com.jcxx.saas.common.validator;
 
+import com.jcxx.saas.common.exception.SaaSException;
+
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-
-import com.jcxx.saas.common.exception.SaaSException;
-
 import java.util.Set;
 
 /**
  * hibernate-validator校验工具类
- *
+ * <p>
  * 参考文档：http://docs.jboss.org/hibernate/validator/5.4/reference/en-US/html_single/
  */
 public class ValidatorUtils {
@@ -26,16 +25,17 @@ public class ValidatorUtils {
 
     /**
      * 校验对象
-     * @param object        待校验对象
-     * @param groups        待校验的组
-     * @throws SaaSException  校验不通过，则报RRException异常
+     *
+     * @param object 待校验对象
+     * @param groups 待校验的组
+     * @throws SaaSException 校验不通过，则报RRException异常
      */
     public static void validateEntity(Object object, Class<?>... groups)
             throws SaaSException {
         Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
         if (!constraintViolations.isEmpty()) {
             StringBuilder msg = new StringBuilder();
-            for(ConstraintViolation<Object> constraint:  constraintViolations){
+            for (ConstraintViolation<Object> constraint : constraintViolations) {
                 msg.append(constraint.getMessage()).append("<br>");
             }
             throw new SaaSException(msg.toString());
